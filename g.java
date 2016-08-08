f25093c187c7f7e82d653050043f1a60c4d04a8a
gokulsankary <gokuly437@gmail.com>
2016-08-08 07:17:05 -0400

    Create g.java

---
+++ g.java
@@ -0,0 +1,14 @@
+
+public class power1221 {
+public static void main(String[] args) {
+	int a=Integer.parseInt(args[0]);
+	int y=0,r=0,c=a,k=0;
+	int e,f,g,h;
+	//reverse a number 12345 to 54321
+	while(a!=0){
+		r=r*10;
+		r=r+(a%10);
+		a=a/10;
+	}c=r;
+}
+}
